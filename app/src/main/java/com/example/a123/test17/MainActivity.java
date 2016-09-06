a933e3be7927e6ac21675eca99377d1304dcf8b3
wangsheng666 <1028742881@qq.com>
2016-09-06 09:12:26 +0800

    掌握SharedPreferences的使用方法

---
+++ app/src/main/java/com/example/a123/test17/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.a123.test17;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    //SharedPreferences文件名
+    private final static String SharedPreferencesFileName="config";
+
+    //键
+    private final static String Key_UserName="UserName";//用户名
+    private final static String Key_LoginDate="LoginDate";//登录时间
+    private final static String Key_UserType="UserType";
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获得SharedPreferences实例
+        preferences=getSharedPreferences(SharedPreferencesFileName, MODE_PRIVATE);
+        editor=preferences.edit();
+
+        Button btnWrite=(Button)findViewById(R.id.button);
+        Button btnRead=(Button)findViewById(R.id.button2);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //格式化日期，将日期按照年月日时分秒格式转换为字符串形式
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String strLoginDate = simpleDateFormat.format(new Date());
+
+                //写入键值对
+                editor.putString(Key_UserName, "Wang Sheng");
+                editor.putString(Key_LoginDate, strLoginDate);
+                editor.putInt(Key_UserType, 1);
+
+                //提交修改，此处换成commit()也可以
+                editor.apply();
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strUserName = preferences.getString(Key_UserName, null);
+                String strLoginDate = preferences.getString(Key_LoginDate, null);
+                int nUserType=preferences.getInt(Key_UserType,0);
+                if (strUserName != null && strLoginDate != null)
+                    Toast.makeText(MainActivity.this, "用户名:" + strUserName + "登录时间:" + strLoginDate+"用户类型:"+nUserType, Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "无数据", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
